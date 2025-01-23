95c1a0408d32ecacfc2576257d9a85cd314b3637
ChrisMichaudd <chrismichaud@shaw.ca>
2025-01-23 02:38:06 -0700

    working prototype on local dev next commit will be testing push to prod deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
+import prepSmartLogo from "@/components/prepsmartlogos/PrepSmartLogoAllWhite.png";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -42,14 +43,18 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
+                    <Link href="/" aria-label="Go to home">
+                      <img
+                        src={prepSmartLogo.src}
+                        alt="PrepSmart Logo"
+                        className="h-16 w-auto"
+                      />
+                    </Link>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
               </nav>
+
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
               </div>
